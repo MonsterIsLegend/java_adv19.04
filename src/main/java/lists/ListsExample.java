1cad8424f45c6b38fe11b5b2b6cc5328e9b0522a
Janek <jaaanek99@gmail.com>
2021-04-20 11:36:49 +0200

    :)))))

---
+++ src/main/java/lists/ListsExample.java
@@ -0,0 +1,46 @@
+package lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListsExample {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(); // inicjalizacja i deklaracja pustej listy
+        List<String> names2 = new ArrayList<>(Arrays.asList("ALA", "JAN", "EWA"));
+        names.add("ALA");
+        names.add("OLA");
+        names.add("ELA");
+        names.add("ALA");
+        names.add("ALAN");
+        names.add("ANA");
+        names.add(1, "MICHAL");
+        //dodawanie kolekcji :))
+        names.addAll(names2);
+        System.out.println(names);
+        //pobieranie
+        System.out.printf("index: %d wartość %s\n", 5, names.get(3));
+        System.out.printf("index: %d wartość %s\n", names.size() - 2, names.get(names.size() - 2));
+        //edycja
+        names.set(5, "TOMASZ"); // musi być na dlugosci listy
+
+
+
+//        for( String s : names){
+//            System.out.println(s);
+//        }
+        //usuawnie
+        names.remove("ALA");
+        System.out.println(names);
+        names.remove(0);
+        System.out.println(names);
+        names.removeAll(new ArrayList<>(Arrays.asList("ALA"))); // usuwanie z logiką odejmowania zbiorów
+        System.out.println(names);
+        //contain - spr. czy zawiera
+        System.out.println(names.contains("ELA"));
+        System.out.println(names.contains("ALA"));
+
+    }
+
+
+}
---
+++ src/main/java/lists/Statistics.java
@@ -0,0 +1,70 @@
+package lists;
+
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@AllArgsConstructor
+public class Statistics {
+    private List<Integer> elements;
+
+
+    //metoda obliczajaca srednia
+
+    private double getAvg(){
+        int avg = 0;
+        for( int d : elements){
+            avg += d;
+        }
+        return avg/elements.size();
+
+    }
+
+    private int getMax(){
+        int max = elements.get(0);
+        for(int b : elements){
+            if( b > max){
+                max = b;
+            }
+        }
+        return max;
+    }
+    private  int getMin(){
+        int min = elements.get(0);
+        for(int b : elements){
+            if( b < min){
+                min = b;
+            }
+        }
+        return min;
+    }
+
+    private int getMostFrequentElement(){
+        int[] amount = new int[elements.size()];
+        for (int i = 0; i < elements.size(); i++) {
+            for (int j = 0; j < elements.size(); j++) {
+                if(elements.get(j).equals(elements.get(i))){
+                    amount[i] ++;
+                }
+            }
+        }
+        int maxindex = 0;
+        for (int i = 1; i < amount.length; i++) {
+            if(amount[i] > amount[maxindex]){
+                maxindex = i;
+            }
+        }
+        return elements.get(maxindex);
+    }
+
+    public static void main(String[] args) {
+
+        Statistics statistics = new Statistics(Arrays.asList(3,3,4,2,5,1,6,3,0,7,9,0,4,5,8,6,3,2));
+        System.out.println(statistics.getMostFrequentElement());
+
+    }
+
+
+}
