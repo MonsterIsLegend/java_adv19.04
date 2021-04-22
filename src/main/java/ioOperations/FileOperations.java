edb058647f36b7cb292f93ed16be19309566bef5
Janek <jaaanek99@gmail.com>
2021-04-22 11:40:00 +0200

    :)))))

--- src/main/java/ioOperations/FileOperations.java
+++ src/main/java/ioOperations/FileOperations.java
@@ -1,7 +1,6 @@
 package ioOperations;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,24 +9,39 @@ public class FileOperations {
     List<String> notes = new ArrayList<>();
 
     public void addNote(String note ){
+        //Printwriter -> tworzy nowy plik gdy nie występuje, lub dodaje istniejący
         try {
             PrintWriter pw = new PrintWriter("MyNotes.txt");
-            pw.println(note);
+            notes.add(note);                //dodaje notatke do listy
+            for( String s : notes){
+                pw.println(s);          //aktualizuje plik na bazie listy
+            }
             pw.close();
         }catch (FileNotFoundException x){
             x.printStackTrace();
-            System.out.println("Nie znaleziono pliku");
+            System.err.println("Nie znaleziono pliku");
         }
     }
-
+    public void appendNotes( String note ){
+        try {
+            FileWriter fw = new FileWriter(new File("myNotesAppend.txt"),true);
+            fw.append(note+"\n");
+            fw.close();
+        }catch (IOException io){
+            io.printStackTrace();
+            System.err.println("Nie znaleziono pliku: ");
+        }
+    }
     public void readNotes(){
 
     }
 
     public static void main(String[] args) {
         FileOperations fo = new FileOperations();
-
-
+        fo.addNote("Nauka Stream API");
+        fo.addNote("Nauka FileIO");
+        fo.appendNotes("Nauka OOP");
+        fo.appendNotes("Nauka enums");
 
 
     }
